edc99da4bf53ac15138dc710fecbd8928e321c8c
Jean-Christophe Fabre <jean-christophe.fabre@inra.fr>
2018-10-05 10:38:37 +0200

    Add of banner for cookies consent

---
+++ LICENSE.3rdPARTY.md
@@ -0,0 +1,3 @@
+## Third Party components
+
+* Cookie Consent : MIT, see https://github.com/insites/cookieconsent/blob/2b0d675fd9acecfd50751e826794f679b958df98/licence
---
+++ src/theme_readthedocs_custom/js/cookie-banner.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "theme": "classic",
+  "palette": {
+    "popup": {
+      "background": "#5ebbb3",
+      "text": "#fff"
+    },
+    "button": {
+      "background": "#303e4d",
+      "text": "#fff"
+    }
+  },
+  "content": {
+    "message": "This website uses cookies to improve your browsing experience",
+    "dismiss": "OK"
+  },
+  "showLink": false
+})});
